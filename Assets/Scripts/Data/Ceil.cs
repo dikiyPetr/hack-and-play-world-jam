75d4841e9db7d6903b2e979bafa578c3bc46cc2c
Igor Rozhkov <i.rojkov@redneckstudio.com>
2025-07-25 15:40:39 +0300

    add new assets for tilemap configuration

--- Assets/Scripts/Data/Ceil.cs
+++ Assets/Scripts/Data/Ceil.cs
@@ -1,10 +1,18 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Data
 {
     public class Ceil
     {
+        private Vector2Int position;
         private Entity background;
         private List<Entity> foregroud;
+        
+        public Ceil(Vector2Int position, Entity background)
+        {
+            this.position = position;
+            this.background = background;
+        }
     }
 }
---
+++ Assets/Scripts/Tilemap/TilemapPresenter.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Data;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[Serializable]
+public class TileInfo
+{
+    public Tile tile;
+    public EntityType type;
+
+} 
+public class TilemapPresenter : MonoBehaviour
+{
+    private UnityEngine.Tilemaps.Tilemap _tilemap;
+    private Map _map;
+    [SerializeField]
+    private TileInfo[] _tileInfos;
+    [SerializeField]
+    private Vector2Int _humonPos;   
+    [SerializeField]
+    private Vector2Int _demonPos;
+
+    private void Awake()
+    {
+        _tilemap = GetComponentInChildren<UnityEngine.Tilemaps.Tilemap>();
+        FillMap();
+    }
+
+    private void FillMap()
+    {
+        BoundsInt bounds = _tilemap.cellBounds;
+        UnityEngine.Tilemaps.TileBase[] allTiles = _tilemap.GetTilesBlock(bounds);
+        Dictionary<Vector2Int, Ceil> ceils = new();
+        for (int x = bounds.xMin; x < bounds.xMax; x++)
+        {
+            for (int y = bounds.yMin; y < bounds.yMax; y++)
+            {
+                Vector2Int position = new Vector2Int(x, y);
+                UnityEngine.Tilemaps.TileBase tile = allTiles[(x - bounds.xMin) + (y - bounds.yMin) * bounds.size.x];
+
+                if (tile != null)
+                {
+                    TileInfo found = _tileInfos.FirstOrDefault(info => info.tile == tile);
+                    var entity = new Entity();
+                    entity.type = found.type;
+                    var ceil = new Ceil(position,entity);
+                    ceils.Add(position, ceil);
+                }
+            }
+        }
+        _map = new Map(ceils, _humonPos, _demonPos);
+    }
+}
