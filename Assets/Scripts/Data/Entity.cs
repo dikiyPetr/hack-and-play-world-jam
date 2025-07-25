f16d9d6dc468cfa40c1f572092a38b038020f18c
Bibuba <princesakenny98@gmail.com>
2025-07-25 15:00:48 +0300

    add map

---
+++ Assets/Scripts/Data/Entity.cs
@@ -0,0 +1,51 @@
+﻿namespace Data
+{
+    public class Entity
+    {
+        public EntityType type;
+        public bool isInteractable;
+    }
+
+    class Character : Entity
+    {
+        public CharacterType characterType; // Human, Demon, NPC
+    }
+
+    class InteractiveEntity : Entity
+    {
+        public InteractionType interactionType; // Human, Demon, NPC
+    }
+
+    class DangerousEntity : Entity
+    {
+        public DangerousType dangerousType; // Human, Demon, NPC
+    }
+
+    enum CharacterType
+    {
+        Human,
+        Demon
+    }
+
+    public enum EntityType
+    {
+        Groud,
+        Character,
+        Obstacle,
+        Interactive,
+        Dangerous,
+    }
+
+    enum DangerousType
+    {
+        Lava,
+        Mouse,
+        HolyWater,
+    }
+
+    enum InteractionType
+    {
+        Pickup,
+        Portal
+    }
+}
---
+++ Assets/Scripts/Data/Map.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Data
+{
+    public class Map
+    {
+        public Dictionary<Vector2Int, Entity> ceils;
+        public Vector2Int humanPos;
+        public Vector2Int demonPos;
+
+        public Map(Dictionary<Vector2Int, Entity> ceils, Vector2Int humanPos, Vector2Int demonPos)
+        {
+            this.ceils = ceils;
+        }
+
+        void Move(Vector2Int move)
+        {
+            // var humanCell = cells[humanPos.x + move.x, humanPos.y + move.x];
+            // var demonCell = cells[demonPos.x + move.x, demonPos.y + move.x];
+            // if (humanCell.rootEntity.isInteractable)
+            // {
+            //     for (var i = 0; i < humanCell.additionalEntity.Count; i++)
+            //     {
+            //         var additionalEntry = humanCell.additionalEntity[i];
+            //         if (additionalEntry.isInteractable)
+            //         {
+            //         }
+            //     }
+            // }
+            //
+            // if (demonCell.rootEntity.isInteractable)
+            // {
+            //     for (var i = 0; i < demonCell.additionalEntity.Count; i++)
+            //     {
+            //         var additionalEntry = demonCell.additionalEntity[i];
+            //         if (additionalEntry.isInteractable)
+            //         {
+            //         }
+            //     }
+            // }
+        }
+
+        void GetEffect(Entity entity)
+        {
+            // if (entity.isInteractable)
+            // {
+            //     switch (entity.type)
+            //     {
+            //         case EntityType.Obstacle:
+            //             break;
+            //         case EntityType.Groud:
+            //             break;
+            //         case EntityType.Character:
+            //             switch (((Character)entity).characterType)
+            //             {
+            //                 case CharacterType.Human:
+            //                     return Effect.ContactWithHuman;
+            //                 case CharacterType.Demon:
+            //                     return Effect.ContactWithDemon;
+            //             }
+            //
+            //             break;
+            //         case EntityType.Dangerous:
+            //             switch (((DangerousEntity)entity).dangerousType)
+            //             {
+            //                 case DangerousType.Lava:
+            //                     return Effect.ContactWithLava;
+            //                 case DangerousType.Mouse:
+            //                     return Effect.ContactWithMouse;
+            //                 case DangerousType.HolyWater:
+            //                     return Effect.ContactWithHolyWatter;
+            //             }
+            //
+            //             break;
+            //         case EntityType.Interactive:
+            //             switch (((InteractiveEntity)entity).interactionType)
+            //             {
+            //                 case InteractionType.Pickup:
+            //                     break;
+            //                 case InteractionType.Portal:
+            //                     break;
+            //             }
+            //
+            //             break;
+            //         default:
+            //             throw new ArgumentOutOfRangeException();
+            //     }
+            // }
+            //
+            // return Effect.None;
+        }
+    }
+
+    class Effect
+    {
+        public EffectType type;
+    }
+
+    enum EffectType
+    {
+        None,
+        ContactWithDemon,
+        ContactWithHuman,
+        ContactWithLava,
+        ContactWithMouse,
+        ContactWithHolyWatter
+    }
+}
