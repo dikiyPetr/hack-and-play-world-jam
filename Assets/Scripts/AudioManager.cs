0f3ac7353d1cee5cbc1fb34446fd9e24221f6056
Bibuba <princesakenny98@gmail.com>
2025-07-26 18:44:58 +0300

    add sounds

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,6 +5,11 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource step;
     [SerializeField] private AudioSource teleport;
     [SerializeField] private AudioSource ambient;
+    [SerializeField] private AudioSource cat;
+    [SerializeField] private AudioSource merge;
+    [SerializeField] private AudioSource rats;
+    [SerializeField] private AudioSource water;
+    [SerializeField] private AudioSource lava;
 
     void Start()
     {
@@ -26,6 +31,33 @@ public class AudioManager : MonoBehaviour
 
     public void PlayTeleport()
     {
+        teleport.time = 0.7f;
         teleport.Play();
+    }
+
+    public void PlayRats()
+    {
+        rats.Play();
+    }
+
+    public void PlayWater()
+    {
+        water.Play();
+    }
+
+    public void PlayLava()
+    {
+        lava.Play();
+    }
+
+    public void PlayMerge()
+    {
+        merge.time = 0.5f;
+        merge.Play();
+    }
+
+    public void PlayCat()
+    {
+        cat.Play();
     }
 }
